a085fb804fc15199792b37eec616bfa8f518ecc0
tochi86 <tochi86.ham@gmail.com>
2022-01-22 21:37:19 +0900

    LoginViewModelとLoginUiStateを追加

---
+++ LoginChallenge/Login/LoginUiState.swift
@@ -0,0 +1,12 @@
+//
+//  LoginUiState.swift
+//  LoginChallenge
+//
+//  Created by Toshiya Kobayashi on 2022/01/22.
+//
+
+import Foundation
+
+struct LoginUiState {
+
+}
---
+++ LoginChallenge/Login/LoginViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModel.swift
+//  LoginChallenge
+//
+//  Created by Toshiya Kobayashi on 2022/01/22.
+//
+
+import Foundation
+
+final class LoginViewModel {
+
+}
